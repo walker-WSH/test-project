6f90d2d86f191f59e829eb61591ed2021acacef0
wangshaohui <wang.shaohui@navercorp.com>
2021-09-29 10:16:40 +0800

    test

--- build.bat
+++ build.bat
@@ -1,5 +1,8 @@
 @echo off
 
 echo start building...................
+
 devenv test.sln /Rebuild "Release|x64"
+if not "%ERRORLEVEL%" == "0" echo building error!
+
 echo build finished...................
